907d1bf686bfb949ec394d3f71553b7fecd41e73
Marti Bolivar <marti@opensourcefoundries.com>
2017-11-27 14:01:34 -0500

    [OSF fromlist] scripts: runner: dfu-util: support DT-based flashing for DfuSe
    Enable DT support in the dfu-util flasher when the target is a
    DfuSe (DFU + ST extensions) device. This requires untangling
    DfuSe-specific options (currently, the default is 'leave', to
    immediately start running after the flashing is done) from the actual
    address itself. The address and options must be combined on the
    dfu-util command line itself.
    
    This lets us fix an unnecessary printline when flashing DfuSe devices
    if 'leave' is given as an option. When that happens (and that's the
    default behavior), there's no need to reset the device.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- scripts/support/runner/dfu.py
+++ scripts/support/runner/dfu.py
@@ -4,25 +4,51 @@
 
 '''Runner for flashing with dfu-util.'''
 
+import argparse
+from collections import namedtuple
+import os
 import sys
 import time
 
-from .core import ZephyrBinaryRunner, RunnerCaps
+from .core import ZephyrBinaryRunner, RunnerCaps, BuildConfiguration
+
+
+class DfuSeOptionsAction(argparse.Action):
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # This implies --dfuse.
+        setattr(namespace, 'dfuse', True)
+
+        # Append the given option to the list, creating it if necessary.
+        opts = getattr(namespace, 'dfuse_option')
+        if opts is None:
+            opts = []
+        opts.append(values)
+        setattr(namespace, 'dfuse_option', opts)
+
+
+DfuSeConfig = namedtuple('DfuSeConfig', ['address', 'options'])
 
 
 class DfuUtilBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end for dfu-util.'''
 
-    def __init__(self, pid, alt, img, dfuse=None, exe='dfu-util', debug=False):
+    def __init__(self, pid, alt, img, exe='dfu-util',
+                 dfuse_config=None, debug=False):
         super(DfuUtilBinaryRunner, self).__init__(debug=debug)
         self.alt = alt
         self.img = img
-        self.dfuse = dfuse
         self.cmd = [exe, '-d,{}'.format(pid)]
         try:
             self.list_pattern = ', alt={},'.format(int(self.alt))
         except ValueError:
             self.list_pattern = ', name="{}",'.format(self.alt)
+
+        if dfuse_config is None:
+            self.dfuse = False
+        else:
+            self.dfuse = True
+        self.dfuse_config = dfuse_config
 
     @classmethod
     def name(cls):
@@ -30,7 +56,7 @@ class DfuUtilBinaryRunner(ZephyrBinaryRunner):
 
     @classmethod
     def capabilities(cls):
-        return RunnerCaps(commands={'flash'})
+        return RunnerCaps(commands={'flash'}, flash_addr=True)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -43,16 +69,29 @@ class DfuUtilBinaryRunner(ZephyrBinaryRunner):
                             help="binary to flash")
 
         # Optional:
-        parser.add_argument("--dfuse-addr", default=None,
-                            help='''target address if the board is a DfuSe
-                            device; ignored it not present''')
+        parser.add_argument("--dfuse", default=False, action='store_true',
+                            help='''set if target is a DfuSe device;
+                            implies --dt-flash.''')
+        parser.add_argument("--dfuse-option", default=None,
+                            action=DfuSeOptionsAction,
+                            help='''add a DfuSe-specific option (such as
+                            'leave') to the flash command; implies --dfuse
+                            (and thus also --dt-flash), and may be given
+                            several times for multiple options''')
         parser.add_argument('--dfu-util', default='dfu-util',
                             help='dfu-util executable; defaults to "dfu-util"')
 
     @classmethod
     def create_from_args(cls, args):
+        if args.dfuse:
+            args.dt_flash = True  # --dfuse implies --dt-flash.
+            build_conf = BuildConfiguration(os.getcwd())
+            dcfg = DfuSeConfig(address=cls.get_flash_address(args, build_conf),
+                               options=args.dfuse_option)
+        else:
+            dcfg = None
         return DfuUtilBinaryRunner(args.pid, args.alt, args.img,
-                                   dfuse=args.dfuse_addr, exe=args.dfu_util,
+                                   exe=args.dfu_util, dfuse_config=dcfg,
                                    debug=args.verbose)
 
     def find_device(self):
@@ -62,17 +101,26 @@ class DfuUtilBinaryRunner(ZephyrBinaryRunner):
         return self.list_pattern in output
 
     def do_run(self, command, **kwargs):
-        reset = 0
+        reset = False
         if not self.find_device():
-            reset = 1
+            reset = True
             print('Please reset your board to switch to DFU mode...')
             while not self.find_device():
                 time.sleep(0.1)
 
         cmd = list(self.cmd)
-        if self.dfuse is not None:
-            cmd.extend(['-s', '{}:leave'.format(self.dfuse)])
+        if self.dfuse:
+            # http://dfu-util.sourceforge.net/dfuse.html
+            dcfg = self.dfuse_config
+            addr_opts = [hex(dcfg.address)] + list(dcfg.options)
+            cmd.extend(['-s', ':'.join(addr_opts)])
         cmd.extend(['-a', self.alt, '-D', self.img])
         self.check_call(cmd)
-        if reset:
+
+        if (reset and not self.dfuse) or ('leave' not in dcfg.options):
+            # Normal DFU devices generally need to be reset to switch
+            # back to the flashed program.
+            #
+            # DfuSe targets do as well, except when 'leave' is given
+            # as an option.
             print('Now reset your board again to switch back to runtime mode.')
