8cbcd34cee8fff22ee51b987a94332b71ad55b20
Marti Bolivar <marti@opensourcefoundries.com>
2017-12-11 12:13:55 -0500

    Revert "[OSF toup] scripts: runner: abstract openocd load/verify"
    This reverts commit 945eccb90d97033144179c056ccb9f92514ea728, as the
    series it's part of needs an update.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- boards/arc/quark_se_c1000_ss_devboard/board.cmake
+++ boards/arc/quark_se_c1000_ss_devboard/board.cmake
@@ -1,4 +1,5 @@
-set(OPENOCD_PRE_CMD        targets 1)
-set(OPENOCD_USE_LOAD_IMAGE y)
+set(OPENOCD_PRE_CMD  targets 1)
+set(OPENOCD_LOAD_CMD load_image ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME} ${CONFIG_FLASH_BASE_ADDRESS})
+set(OPENOCD_VERIFY_CMD verify_image ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME} ${CONFIG_FLASH_BASE_ADDRESS})
 
 include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
