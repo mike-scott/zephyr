ddd3d22856e2fd21800465b58a6d84787199f2e6
Marti Bolivar <marti@opensourcefoundries.com>
2018-05-18 16:42:56 -0400

    Revert "[OSF toup] net: coap: clear more fields in coap_reply_clear()"
    This reverts commit 74ac1f9feb9a7363194d62fac49f692f2993d239, which
    was merged upstream.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- subsys/net/lib/coap/coap.c
+++ subsys/net/lib/coap/coap.c
@@ -790,8 +790,6 @@ void coap_reply_clear(struct coap_reply *reply)
 	reply->id = 0;
 	reply->tkl = 0;
 	reply->reply = NULL;
-	reply->user_data = NULL;
-	reply->age = 0;
 }
 
 int coap_resource_notify(struct coap_resource *resource)
