d365489abd7dd21f4debbd60b9829967302c8fc7
Marti Bolivar <marti@foundries.io>
2019-02-11 12:40:56 -0700

    Revert "[FIO fromtree] logging: introduce LOG_DISABLE_FANCY_OUTPUT_FORMATTING to save flash"
    This reverts commit 7f3f85dcf3df4c74d9669417a87cb697c14b3d8b, which
    was merged upstream.
    
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- subsys/logging/log_output.c
+++ subsys/logging/log_output.c
@@ -114,8 +114,7 @@ static int print_formatted(const struct log_output *log_output,
 	int length = 0;
 
 	va_start(args, fmt);
-#if !defined(CONFIG_NEWLIB_LIBC) && !defined(CONFIG_ARCH_POSIX) && \
-    !defined(CONFIG_LOG_DISABLE_FANCY_OUTPUT_FORMATTING)
+#if !defined(CONFIG_NEWLIB_LIBC) && !defined(CONFIG_ARCH_POSIX)
 	length = _prf(out_func, (void *)log_output, (char *)fmt, args);
 #else
 	_vprintk(out_func, (void *)log_output, fmt, args);
@@ -552,8 +551,7 @@ void log_output_string(const struct log_output *log_output,
 				level, domain_id, source_id);
 	}
 
-#if !defined(CONFIG_NEWLIB_LIBC) && !defined(CONFIG_ARCH_POSIX) && \
-    !defined(CONFIG_LOG_DISABLE_FANCY_OUTPUT_FORMATTING)
+#if !defined(CONFIG_NEWLIB_LIBC) && !defined(CONFIG_ARCH_POSIX)
 	length = _prf(out_func, (void *)log_output, (char *)fmt, ap);
 #else
 	_vprintk(out_func, (void *)log_output, fmt, ap);
