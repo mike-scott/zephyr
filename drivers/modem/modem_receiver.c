ea891aebbd8fd4d47321bfd1cecb51192ec8cebb
Michael Scott <mike@foundries.io>
2019-03-06 15:45:32 -0800

    Revert "[FIO fromlist] drivers: modem: receiver: fix sending"
    Merged upstream.
    
    This reverts commit ca875602101a908c4252bf6f06ae50f3ebff00dd.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- drivers/modem/modem_receiver.c
+++ drivers/modem/modem_receiver.c
@@ -177,19 +177,19 @@ int mdm_receiver_recv(struct mdm_receiver_context *ctx,
 int mdm_receiver_send(struct mdm_receiver_context *ctx,
 		      const u8_t *buf, size_t size)
 {
-	int written;
-
 	if (!ctx) {
 		return -EINVAL;
 	}
 
-	while (size) {
+	while (size)  {
+		int written;
+
 		written = uart_fifo_fill(ctx->uart_dev,
 					 (const u8_t *)buf, size);
 		if (written < 0) {
 			/* error */
 			uart_irq_tx_disable(ctx->uart_dev);
-			return -EIO;
+			return written;
 		} else if (written < size) {
 			k_yield();
 		}
